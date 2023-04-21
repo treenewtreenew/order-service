6ca1d809e2bd35864d0a5e49effa46d3db7dd99b
treenewtreenew <treenewtreenew@gmail.com>
2023-04-21 22:13:46 +0900

    reactive

---
+++ src/main/java/com/polarbookshop/orderService/config/ClientProperties.java
@@ -0,0 +1,12 @@
+package com.polarbookshop.orderService.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+
+@ConfigurationProperties(prefix = "polar")
+public record ClientProperties(
+        @NotNull
+        URI catalogServiceUri
+) {}
---
+++ src/main/java/com/polarbookshop/orderService/order/web/OrderController.java
@@ -0,0 +1,29 @@
+package com.polarbookshop.orderService.order.web;
+
+import com.polarbookshop.orderService.order.domain.Order;
+import com.polarbookshop.orderService.order.domain.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+    private final OrderService orderService;
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+    public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+
+}
