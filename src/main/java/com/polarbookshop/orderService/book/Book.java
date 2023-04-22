ba67db986fd4a32cf4e34dee92b85d623a11dee5
treenewtreenew <treenewtreenew@gmail.com>
2023-04-22 23:20:29 +0900

    reactive

---
+++ src/main/java/com/polarbookshop/orderService/book/Book.java
@@ -0,0 +1,8 @@
+package com.polarbookshop.orderService.book;
+
+public record Book(
+        String isbn,
+        String title,
+        String author,
+        Double price
+) {}
---
+++ src/main/java/com/polarbookshop/orderService/config/ClientConfig.java
@@ -0,0 +1,7 @@
+package com.polarbookshop.orderService.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClientConfig {
+}
